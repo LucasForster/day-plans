3ac4dffe1597defd130a9eb573f3435b2ccbfb22
Lucas Forster <lucas.forster@lehmenkaut.de>
2021-05-24 16:15:32 +0200

    :sparkles: Implement basic District struct and parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
+use std::convert::TryInto;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
 
 fn main() {
-    print!("{}", read_ascii_file(Path::new("verkehrsfluss/verkehrsfluss-zusatz/qz-gebiet-nl.dat")));
+    for district in load_districts().iter() {
+        println!("{:?}", district);
+    }
+}
+
+const DISTRICT_COUNT: usize = 647;
+
+#[derive(Debug)]
+struct District {
+    id: u16,
+}
+
+fn load_districts() -> [District; DISTRICT_COUNT] {
+    let path = Path::new("verkehrsfluss/verkehrsfluss-zusatz/qz-gebiet-nl.dat");
+    let data = read_ascii_file(&path);
+    let mut reader = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .delimiter(b'\t')
+        .from_reader(data.as_bytes());
+    let mut districts : Vec<District> = Vec::new();
+    for result in reader.records() {
+        let record = result.unwrap();
+        districts.push(District {
+            id: record[0].parse().unwrap(),
+        });
+    }
+    districts.try_into().unwrap()
 }
 
 fn read_ascii_file(path: &Path) -> String {
