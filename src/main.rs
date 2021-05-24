c6524b8b7ca35a16081dd7a0a4a092bd1e71bf58
Lucas Forster <lucas.forster@lehmenkaut.de>
2021-05-24 13:15:13 +0200

    :label: Replace with more appropriate types

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use std::fs::File;
 use std::io::Read;
+use std::path::Path;
 
 
 fn main() {
-    print!("{}", read_ascii_file("verkehrsfluss/verkehrsfluss-zusatz/qz-gebiet-nl.dat".to_string()));
+    print!("{}", read_ascii_file(Path::new("verkehrsfluss/verkehrsfluss-zusatz/qz-gebiet-nl.dat")));
 }
 
-fn read_ascii_file(path: String) -> String {
-    let mut file = File::open(path).unwrap();
+fn read_ascii_file(path: &Path) -> String {
+    let mut file = File::open(&path).unwrap();
     let mut data = Vec::new();
     file.read_to_end(&mut data).unwrap();
     unsafe {
