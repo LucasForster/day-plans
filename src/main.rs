0d0afb3b56bd3c95f733c516db58a334cddc35ac
Lucas Forster <lucas.forster@lehmenkaut.de>
2021-05-24 11:43:00 +0200

    :sparkles: Parse first file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::fs::File;
+use std::io::Read;
+
+
 fn main() {
-    println!("Hello, world!");
+    print!("{}", read_ascii_file("verkehrsfluss/verkehrsfluss-zusatz/qz-gebiet-nl.dat".to_string()));
+}
+
+fn read_ascii_file(path: String) -> String {
+    let mut file = File::open(path).unwrap();
+    let mut data = Vec::new();
+    file.read_to_end(&mut data).unwrap();
+    unsafe {
+        return std::str::from_utf8_unchecked(&data).to_string();
+    }
 }
