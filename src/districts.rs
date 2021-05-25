fdc3fe6eb5d34be35f1c63d767e1d2d5bbf71915
Lucas Forster <lucas.forster@lehmenkaut.de>
2021-05-25 10:08:25 +0200

    :sparkles: Add map from district id to index

--- src/districts.rs
+++ src/districts.rs
@@ -1,17 +1,26 @@
+use std::collections::HashMap;
 use std::convert::TryInto;
 use std::path::Path;
 
 use super::io;
 
+
+type Id = usize;
+#[derive(Debug)]
+pub struct Index(usize);
+
 const COUNT: usize = 647;
 
 #[derive(Debug)]
 pub struct District {
-    id: u16,
+    x: f64,
+    y: f64,
 }
 
+#[derive(Debug)]
 pub struct Districts {
     districts: [District; COUNT],
+    map: HashMap<Id, Index>,
 }
 
 pub fn load() -> Districts {
@@ -21,14 +30,18 @@ pub fn load() -> Districts {
         .has_headers(false)
         .delimiter(b'\t')
         .from_reader(data.as_bytes());
-    let mut districts : Vec<District> = Vec::new();
+    let mut districts: Vec<District> = Vec::new();
+    let mut map: HashMap<Id, Index> = HashMap::new();
     for result in reader.records() {
         let record = result.unwrap();
+        map.insert(record[0].parse().unwrap(), Index(districts.len()));
         districts.push(District {
-            id: record[0].parse().unwrap(),
+            x: record[1].parse().unwrap(),
+            y: record[2].parse().unwrap(),
         });
     }
     Districts {
         districts: districts.try_into().unwrap(),
+        map,
     }
 }
