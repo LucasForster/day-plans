266d8845d159eae64b7eb67c2d9d9542716a8f23
Lucas Forster <lucas.forster@lehmenkaut.de>
2021-06-13 11:56:06 +0200

    :zap: Replace generator with sum safe rounding

--- src/capacities.rs
+++ src/capacities.rs
@@ -5,6 +5,7 @@ use super::{
     time_bins, time_bins::TimeBin, time_bins::TIME_BINS,
     trips::Trip, trips::TRIPS,
 };
+use std::convert::TryInto;
 
 
 type Count = usize;
@@ -16,32 +17,15 @@ pub struct Capacities {
 }
 impl Capacities {
     pub fn new() -> Capacities {
+        let of_trips = TRIPS.iter().map(|trip| trip.count).collect();
         let mut of_levels: Vec<[Count; time_bins::COUNT]> = Vec::new();
-        {
-            for category in CATEGORIES.iter() {
-                assert!(category.index == of_levels.len());
-                let mut generator: Generator<TimeBin> = Generator::new(
-                    TIME_BINS.into_iter().copied().zip(levels::get_levels(category).to_vec().into_iter()).collect());
-                let mut counts = [0 as Count; time_bins::COUNT];
-                let total_trip_count: usize = TRIPS.iter().filter(|&trip| trip.category.eq(category)).map(|trip| trip.count).sum();
-                for _ in 0..total_trip_count {
-                    counts[generator.next().unwrap().value()] += 1;
-                }
-            }
+        for category in CATEGORIES.iter() {
+            let total = TRIPS.iter().filter(|&trip| trip.category.eq(category)).count() as f64;
+            let values = levels::get_levels(category).iter().map(|share| share * total).collect::<Vec<f64>>();
+            of_levels.push(sum_safe_round(&values).try_into().unwrap());
         }
-        let mut of_modes: Vec<Count> = vec![0; MODES.len()];
-        {
-            let input: Vec<(&Mode, Share)> = MODES.iter().map(|mode| (mode, mode.share)).collect();
-            let mut generator = Generator::new(input);
-            for mode in generator.take(TRIPS.len()) {
-                of_modes[mode.index] += 1;
-            }
-        }
-        Capacities {
-            of_trips: TRIPS.iter().map(|trip| trip.count).collect(),
-            of_levels,
-            of_modes,
-        }
+        let of_modes: Vec<Count> = sum_safe_round(&MODES.iter().map(|mode| mode.share * (TRIPS.len() as f64)).collect());
+        Capacities { of_trips, of_levels, of_modes }
     }
     pub fn of_trip(&self, trip: &Trip) -> Count {
         self.of_trips[trip.index]
@@ -53,40 +37,13 @@ impl Capacities {
 }
 
 
-type Share = f64;
-
-struct Generator<T: Copy> {
-    elements: Vec<T>,
-    distribution: Vec<Share>,
-    counts: Vec<usize>,
-}
-impl<T: Copy> Generator<T> {
-    fn new(input: Vec<(T, Share)>) -> Generator<T> {
-        Generator {
-            elements: input.iter().map(|entry| entry.0).collect(),
-            distribution: input.iter().map(|entry| entry.1).collect(),
-            counts: vec![0; input.len()],
-        }
-    }
-}
-impl<T: Copy> Iterator for Generator<T> {
-    type Item = T;
-    fn next(&mut self) -> Option<T> {
-        if self.elements.is_empty() {
-            return None;
-        }
-        let total: usize = self.counts.iter().sum();
-        let target_counts: Vec<f64> = self.distribution.iter().map(|share| share * ((total + 1) as f64)).collect();
-        let mut index_max: usize = 0;
-        let mut diff_max: f64 = target_counts[index_max] - (self.counts[index_max] as f64);
-        for i in 1..self.elements.len() {
-            let diff_i = target_counts[i] - (self.counts[i] as f64);
-            if diff_i > diff_max {
-                index_max = i;
-                diff_max = diff_i;
-            }
-        }
-        self.counts[index_max] += 1;
-        Some(self.elements[index_max])
-    }
+fn sum_safe_round(values: &Vec<f64>) -> Vec<usize> {
+    let sum: usize = values.iter().sum::<f64>().round() as usize;
+    let mut round_values: Vec<usize> = values.iter().map(|x| x.floor() as usize).collect();
+    let mut enumerated_diff: Vec<(usize, f64)> = values.iter().enumerate().map(|(i, x)| (i, x - (round_values[i] as f64))).collect();
+    enumerated_diff.sort_by(|(_, x), (_, y)| y.partial_cmp(x).unwrap()); // desc
+    let sum_diff: usize = sum - round_values.iter().sum::<usize>();
+    enumerated_diff.iter().take(sum_diff).for_each(|&(i, _)| round_values[i] += 1);
+    assert!(round_values.iter().sum::<usize>() == sum);
+    round_values
 }
