a9b1df3a02edb345d2e15592cff86212e96fdbc6
Lucas Forster <lucas.forster@lehmenkaut.de>
2021-06-23 19:29:30 +0200

    :art: Use cartesian product of three iterators

--- src/graph.rs
+++ src/graph.rs
@@ -3,6 +3,7 @@ use super::modes::{Mode, MODES};
 use super::purposes::Purpose;
 use super::time_bins::{TimeBin, TIME_BINS};
 use super::trips::{Trip, TRIPS};
+use itertools::Itertools;
 use petgraph::graph::{EdgeIndex, Graph as Petgraph, NodeIndex};
 use petgraph::Direction::Outgoing;
 use std::collections::HashMap;
@@ -23,34 +24,34 @@ impl Graph {
     pub fn new() -> Self {
         let mut graph = Petgraph::<Node, Edge>::new();
         let mut nodes = HashMap::<Node, NodeIndex>::new();
-        for trip in TRIPS.iter() {
-            for time_bin in TIME_BINS.into_iter().copied() {
-                for mode in MODES.iter() {
-                    let trip_category = trip.category;
-                    let source_key = Node {
-                        district_id: trip.origin.id,
-                        purpose: trip_category.origin,
-                        time_bin,
-                    };
-                    if !nodes.contains_key(&source_key) {
-                        nodes.insert(source_key, graph.add_node(source_key));
-                    }
-                    let source_index: NodeIndex = *nodes.get(&source_key).unwrap();
-
-                    let destination_key = Node {
-                        district_id: trip.destination.id,
-                        purpose: trip_category.destination,
-                        time_bin: time_bin + trip_category.origin.duration(), // TODO: leg duration
-                    };
-                    if !nodes.contains_key(&destination_key) {
-                        nodes.insert(destination_key, graph.add_node(destination_key));
-                    }
-                    let destination_index: NodeIndex = *nodes.get(&destination_key).unwrap();
-
-                    let edge_key = Edge { trip, mode };
-                    graph.add_edge(source_index, destination_index, edge_key);
-                }
+        for ((trip, time_bin), mode) in TRIPS
+            .iter()
+            .cartesian_product(TIME_BINS.into_iter().copied())
+            .cartesian_product(MODES.iter())
+        {
+            let trip_category = trip.category;
+            let source_key = Node {
+                district_id: trip.origin.id,
+                purpose: trip_category.origin,
+                time_bin,
+            };
+            if !nodes.contains_key(&source_key) {
+                nodes.insert(source_key, graph.add_node(source_key));
             }
+            let source_index: NodeIndex = *nodes.get(&source_key).unwrap();
+
+            let destination_key = Node {
+                district_id: trip.destination.id,
+                purpose: trip_category.destination,
+                time_bin: time_bin + trip_category.origin.duration(), // TODO: leg duration
+            };
+            if !nodes.contains_key(&destination_key) {
+                nodes.insert(destination_key, graph.add_node(destination_key));
+            }
+            let destination_index: NodeIndex = *nodes.get(&destination_key).unwrap();
+
+            let edge_key = Edge { trip, mode };
+            graph.add_edge(source_index, destination_index, edge_key);
         }
         graph.shrink_to_fit();
         println!("\rBuilt graph: {} nodes, {:.2e} edges", graph.node_count(), graph.edge_count());
